1c6aed9bd8d85c8e1eacb44ab57e7007611059d5
rivka123 <gasasa255@gmail.com>
2022-04-07 09:34:42 +0300

    up

--- poewrball.py
+++ poewrball.py
@@ -1,14 +1,13 @@
 import random
 from function import winnerlist
 import colorama
-# from function import comparison
 
 w=[]
 g=[]
 white=[]
 gold=[]
 winnerlist(w,g,white,gold)
-# comparison(w,white,g,gold)
+
 
 
 
